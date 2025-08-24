89d293cb495b146450cea420d619e242a29c29a5
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-08-24 19:05:37 +0200

    init project

---
+++ src/test/java/mateusz/spring_in_action/SprintInActionApplicationTest.java
@@ -0,0 +1,13 @@
+package mateusz.spring_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/mateusz/spring_in_action/tacos/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package mateusz.spring_in_action.tacos;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest (HomeController.class) // specifies web test for HomeController
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; // injects MockMvc
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) // expects HTTP 200
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+}
